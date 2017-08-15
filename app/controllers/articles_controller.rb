0ba0f9ded0dd9426bba35666a67db638eeb7a700
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-08-15 11:13:52 +0800

    add order by created_at to group's post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comments = @article.comments.order("created_at DESC")
   end
 
   def edit
