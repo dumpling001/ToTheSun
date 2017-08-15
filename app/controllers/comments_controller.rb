d530d2567f3f941a709d9204f6049ea5a351520f
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-08-15 11:00:51 +0800

    implement post create function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new(comment_params)
+    @comment.article = @article
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
