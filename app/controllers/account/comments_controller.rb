06fde9f3d69c88e1370c4658722c30e601a40d4e
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-08-15 14:27:20 +0800

    implement my comments

---
+++ app/controllers/account/comments_controller.rb
@@ -0,0 +1,8 @@
+class Account::CommentsController < ApplicationController
+ before_action :authenticate_user!
+
+ def index
+   @comments = current_user.comments
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
  namespace :account do
    resources :articles
+   resources :comments
  end
 
  root 'articles#index'
